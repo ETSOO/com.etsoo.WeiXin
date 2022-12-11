be154b303ccf4f6041908fe53da269d6a66dfe95
Garry Xiao <xz@etsoo.com>
2022-12-11 20:46:42 +1300

    Upgrade to .NET 7

---
+++ com.etsoo.WeiXin/RQ/CreateJsApiSignatureRQ.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace com.etsoo.WeiXin.RQ
+{
+    /// <summary>
+    /// Create WeiXin JS siganture request data
+    /// 创建微信 Js 接口签名请求数据
+    /// </summary>
+    public class CreateJsApiSignatureRQ
+    {
+        /// <summary>
+        /// Signed Uri
+        /// 签名地址
+        /// </summary>
+        [Required]
+        public string Url { get; set; } = null!;
+    }
+}
